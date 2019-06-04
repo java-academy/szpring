79c63254beb3b7bd16565da46c3506e3a245d3f6
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-06-04 15:19:23 +0200

    Create Awarnes explenation

---
+++ src/main/java/autowire/F_Aware/ŚwiadomeZiarnoAdnotacja.java
@@ -0,0 +1,20 @@
+package autowire.F_Aware;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marcin Ogorzalek
+ */
+@Component
+public class ŚwiadomeZiarnoAdnotacja {
+
+  @Autowired
+  ApplicationContext applicationContextAdnotacja;
+
+  void użycieZiarna() {
+    System.out.println("Metoda z ŚwiadomeZiarnoAdnotacja");
+    applicationContextAdnotacja.getBean(Ziarno.class).metodaZiarenka();
+  }
+}
---
+++ src/main/java/autowire/F_Aware/ŚwiadomeZiarnoIntaface.java
@@ -0,0 +1,26 @@
+package autowire.F_Aware;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marcin Ogorzalek
+ */
+@Component
+public class ŚwiadomeZiarnoIntaface implements ApplicationContextAware {
+
+  ApplicationContext applicationContextInterface;
+
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    this.applicationContextInterface = applicationContext;
+    System.out.println("ŚwiadomeZiarnoIntaface setter");
+  }
+
+  void użycieZiarna() {
+    System.out.println("Metoda z ŚwiadomeZiarnoIntaface");
+    applicationContextInterface.getBean(Ziarno.class).metodaZiarenka();
+  }
+}
