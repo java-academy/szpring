9913fc639b85eff36510a6d1b58cf8d3d396796d
WIktor <wiktor-rup@wp.pl>
2019-06-04 11:00:49 +0200

    Implementacja wykorzytania adnotacji Required
    Dodana klasa Samochod reprezentujaca fasolke
    Dodany plik Beans.xml z konfiguracja kontekstu oraz fasolki

---
+++ src/main/java/adnotacje/Main.java
@@ -0,0 +1,20 @@
+package adnotacje;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Klasa rozruchowa aplikacji. Tworzony jest kontekst aplikacji springowej. {@link ApplicationContext}
+ *
+ * @author Wiktor Rup
+ */
+class Main {
+
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        Samochód samochód = (Samochód) context.getBean("samochód");
+        System.out.println("Rok produkcji: " + samochód.getRokProdukcji());
+        System.out.println("Marka: " + samochód.getMarka());
+    }
+}
---
+++ src/main/java/adnotacje/Samochód.java
@@ -0,0 +1,36 @@
+package adnotacje;
+
+
+import org.springframework.beans.factory.annotation.Required;
+
+/**
+ * Klasa, która obrazuje działanie adnotacji Required {@link org.springframework.beans.factory.annotation.Required}
+ * Od wersji 5.1 adnotacja @Required jest adnotacją przestarzałą z racji na to,
+ * że wymagane zależności powinny być wstrzykiwane przez konstruktor.
+ *
+ * Do prezentacji działania adnotacji Required zmienić wersję Springa na 5.0.9.
+ * @author Wiktor Rup
+ */
+class Samochód {
+
+    private Integer rokProdukcji;
+    private String marka;
+
+    Integer getRokProdukcji() {
+        return rokProdukcji;
+    }
+
+    @Required
+    public void setRokProdukcji(Integer rokProdukcji) {
+        this.rokProdukcji = rokProdukcji;
+    }
+
+    String getMarka() {
+        return marka;
+    }
+
+    @Required
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+}
