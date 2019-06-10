9f62254fa7421546d8c42aec0a20c0b40a813ce4
Ozonek95 <kacperstaszek1995@gmail.com>
2019-06-10 15:43:45 +0200

    Zrobione D_init_and_destroy packageInfo

--- src/main/java/autowire/D_init_and_destroy/Main.java
+++ src/main/java/autowire/D_init_and_destroy/Main.java
@@ -5,18 +5,10 @@ import autowire.D_init_and_destroy.api.RepozytoriumUżytkowników;
 import org.springframework.context.ApplicationContext;
 
 /**
+ * WAŻNE: W celu uzyskania instrukcji umieść kursor na D_init_and_destroy
+ * (pierwsza linijka tego pliku) i wciśnij ctrl+q
+ *
  * @author Kacper Staszek
- *
- * TODO:
- *  1. Stwórz konfigurację do podanego szkieletu klas. Połącz wszytkie trzy typy konfiguracji.
- *  (repozytorium przez annotacje, dwa loggery z konfiguracji java, jeden z XML)
- *  2. Na 3 różne sposoby zaimplementuj metody init i destroy.
- *  - https://www.mkyong.com/spring/spring-init-method-and-destroy-method-example/
- *  - https://www.mkyong.com/spring/spring-postconstruct-and-predestroy-example/
- *  - https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/beans/factory/InitializingBean.html
- *  - https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/beans/factory/DisposableBean.html
- *  Na potrzeby konfiguracji w XML stwórz plik konfiguracyjny, który osiągnie cel. (nazwij go konfiguracja.xml).
- *
  */
 class Main {
 
@@ -44,6 +36,5 @@ class Main {
     repozytorium.setLogger(licznikLogger);
 
     repozytorium.zapisz(new Użytkownik("Bez niespodzianek - Kacper"));
-
   }
 }
