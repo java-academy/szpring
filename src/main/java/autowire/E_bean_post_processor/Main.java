80b7b0c8ed9944568e9f9425cca4b98ef773a935
Ozonek95 <kacperstaszek1995@gmail.com>
2019-06-10 16:05:24 +0200

    package info E_bean_post_processor

--- src/main/java/autowire/E_bean_post_processor/Main.java
+++ src/main/java/autowire/E_bean_post_processor/Main.java
@@ -5,29 +5,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 
 /**
- * Ćwiczenie pokaże Ci jak działa BeanPostProcessor, odświeży kolejność wykonywania operacji
- * przy tworzeniu ziaren, oraz refleksję.
+ * WAŻNE: W celu uzyskania instrukcji
+ * umieść kursor na E_bean_post_processor (pierwsza linijka tego pliku).
  *
  * @author Kacper Staszek
- *
- *  TODO: Uruchom program, zobaczy wydruk na konsoli.
- *
- *  TODO: Zakomentuj wskazaną linię w klasie DrugiSuperTypImpl, po czym uruchom program jeszcze raz.
- *    następnie odkomentuj linię.
- *    Odpowiedz dlaczego jest różnica w działaniu programu.
- *
- *  TODO: Twoim zadaniem jest uzyskać następujący wydruk na konsoli po uruchomieniu programu:
- *    Niezmienione
- *    Niezmienione
- *    Zmienione!
- *
- *  TODO:
- *    Żeby to osiągnąć musisz napisać własny BeanPostProcessor, w którym nadpisz odpowiednią metodę.
- *    BeanPostProcessory mogą przetwożyć każde tworzone ziarno przed ustawieniem wszystkich właściwości i po nim.
- *    W nadpisanej metodzie za pomocą refleksji zmień wartość pola myString na "Zmienione!".
- *
- * https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/beans/factory/config/BeanPostProcessor.html
- * https://www.tutorialspoint.com/spring/spring_bean_post_processors.htm
  */
 @ComponentScan(basePackages = "autowire.E_bean_post_processor")
 class Main {
