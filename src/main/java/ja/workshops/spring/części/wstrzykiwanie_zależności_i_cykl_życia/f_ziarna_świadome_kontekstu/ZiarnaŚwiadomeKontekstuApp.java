6bb3a13b4addc954c8336d9e0e8ff7f6f15f5720
Tomasz "LAFK_pl" Borek <Tomasz.Borek@gmail.com>
2022-04-11 21:55:18 +0200

    Odpowiedzi na pytania kodem - pakiet f rozwiązany
    If you can’t explain something to a first year student, then you haven’t really understood it.
        -- Richard Feynman

--- src/main/java/ja/workshops/spring/części/wstrzykiwanie_zależności_i_cykl_życia/f_ziarna_świadome_kontekstu/ZiarnaŚwiadomeKontekstuApp.java
+++ src/main/java/ja/workshops/spring/części/wstrzykiwanie_zależności_i_cykl_życia/f_ziarna_świadome_kontekstu/ZiarnaŚwiadomeKontekstuApp.java
@@ -4,6 +4,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 /**
  * <p><b>WAŻNE: Aby uzyskać instrukcję umieść kursor na f_ziarna_świadome_kontekstu (sama góra tego pliku) i wyświetl JavaDoc pakietu (w IntelliJu Ctrl+q)</b></p>
  *
@@ -30,9 +33,18 @@ class ZiarnaŚwiadomeKontekstuApp {
     private static void pytania(ApplicationContext kontekst) {
       //TODO: pod każdym pytaniem napisz odpowiedni kawałek kodu
       System.out.println("Ile ziaren jest zdefiniowanych w kontekście?");
+      System.out.println("\t" + kontekst.getBeanDefinitionCount());
       System.out.println("Jak nazywa się ten kontekst?");
+      System.out.println("\t podobno przyjazna ta nazwa być powinna a niezbyt jest. Ale co ja tam wiem: " + kontekst.getDisplayName());
       System.out.println("Kiedy wystartował ten kontekst?");
+      System.out.println("\t wystartował o (uwzględnia strefę): " + Instant.ofEpochMilli(kontekst.getStartupDate()).plus(2, ChronoUnit.HOURS));
       System.out.println("A kiedy wystartował jego rodzic?");
+      System.out.println("\tNie ma rodzica - getParent zwróci null");
       System.out.println("Jak wyciągnąć z kontekstu ziarno świadome swego imienia?");
+      final ZiarnoŚwiadomeImienia zswi = kontekst.getBean(ZiarnoŚwiadomeImienia.class);
+      System.out.println(zswi.nazwaZiarna);
+      zswi.setBeanName("jakieś tam ziarno");
+      System.out.println(zswi.nazwaZiarna);
+      System.out.println("Na ponownie wyciągniętym ziarnie? " + kontekst.getBean(ZiarnoŚwiadomeImienia.class).nazwaZiarna);
     }
 }
